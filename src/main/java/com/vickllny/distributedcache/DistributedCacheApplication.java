4915a75d0a176094cd4b6bb748fbc958ee40d7f9
vickllny <vickllny@163.com>
2024-04-20 00:15:37 +0800

    ByteBuddy Class

--- src/main/java/com/vickllny/distributedcache/DistributedCacheApplication.java
+++ src/main/java/com/vickllny/distributedcache/DistributedCacheApplication.java
@@ -4,22 +4,27 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
+import com.baomidou.mybatisplus.core.toolkit.ClassUtils;
 import com.google.common.hash.Hashing;
 import com.vickllny.distributedcache.annotations.EnableASyncTask;
+import com.vickllny.distributedcache.config.SpringUtils;
 import com.vickllny.distributedcache.domain.User;
 import com.vickllny.distributedcache.service.impl.TestService;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.description.annotation.AnnotationDescription;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.implementation.FieldAccessor;
 import net.bytebuddy.matcher.ElementMatchers;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.mapper.MapperFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.ClassUtils;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Modifier;
@@ -38,6 +43,8 @@ public class DistributedCacheApplication implements CommandLineRunner {
 	private TestService testService;
 	@Autowired
 	private List<BaseMapper> baseMappers;
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DistributedCacheApplication.class, args);
@@ -60,10 +67,10 @@ public class DistributedCacheApplication implements CommandLineRunner {
 				.defineMethod("setPassword", void.class, Modifier.PUBLIC)
 				.withParameters(String.class)
 				.intercept(FieldAccessor.ofField("password"))
+				.name("com.vickllny.distributedcache.domain.User" + StringUtils.capitalize(hash))
 				.make();
 
-		final Class<? extends User> entityClass = dynamicType.load(ClassUtils.class.getClassLoader()).getLoaded();
-
+		final Class<? extends User> entityClass = dynamicType.load(getClass().getClassLoader(), ClassLoadingStrategy.Default.WRAPPER).getLoaded();
 
 
 		//生成mapper
@@ -71,11 +78,15 @@ public class DistributedCacheApplication implements CommandLineRunner {
 				.makeInterface(TypeDescription.Generic.Builder.parameterizedType(BaseMapper.class, entityClass).build())
 				.name(String.format("com.vickllny.distributedcache.mapper.%sMapper", entityClass.getSimpleName()))
 				.make()
-				.load(ClassUtils.class.getClassLoader());
-		final Class<?> mapperClass = mapperLoad
-				.getLoaded();
+				.load(getClass().getClassLoader(), ClassLoadingStrategy.Default.INJECTION);
+		final Class<?> mapperClass = mapperLoad.getLoaded();
 
-		TableInfoHelper.getTableInfo(User.class).getConfiguration().addMapper(mapperClass);
+		MapperFactoryBean<?> factoryBean = new MapperFactoryBean<>(mapperClass);
+		factoryBean.setSqlSessionFactory(sqlSessionFactory);
+
+		sqlSessionFactory.getConfiguration().addMapper(mapperClass);
+
+		SpringUtils.registerBean(getBeanName(mapperClass.getSimpleName()), factoryBean.getObject());
 
 		final User user = entityClass.getConstructor().newInstance();
 		user.setId("12312312312");
@@ -85,6 +96,19 @@ public class DistributedCacheApplication implements CommandLineRunner {
 
 		//测试保存
 		baseMappers.get(0).insert(user);
+	}
+
+	// 根据类名获取 bean name
+	private static String getBeanName(String className) {
+
+		int index = className.lastIndexOf(".");
+		String simpleClassName = index != -1 ? className.substring(index + 1) : className;
+
+		char firstChar = simpleClassName.charAt(0);
+		if (firstChar >= 'A' && firstChar <= 'Z') {
+			firstChar -= 'A' - 'a';
+		}
+		return firstChar + simpleClassName.substring(1);
 	}
 
 	String hash(String input, int length){
---
+++ src/main/java/com/vickllny/distributedcache/config/MapperConfig.java
@@ -0,0 +1,12 @@
+package com.vickllny.distributedcache.config;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AutoConfigureAfter(SpringUtils.class)
+public class MapperConfig {
+
+
+
+}
