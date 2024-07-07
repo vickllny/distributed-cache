84c39c4c4285cdf8c81cf38affabed684111a666
vickllny <vickllny@163.com>
2024-07-07 22:52:43 +0800

    ai ~

--- src/test/java/com/vickllny/distributedcache/GdalReadShpTest.java
+++ src/test/java/com/vickllny/distributedcache/GdalReadShpTest.java
@@ -18,20 +18,32 @@ public class GdalReadShpTest {
     public void read(){
         gdal.AllRegister();
         ogr.RegisterAll();
-        final DataSource dataset = ogr.Open("/Users/vickllny/gis/h48c002002/lrdl.shp");
+        // final DataSource dataset = ogr.Open("/Users/vickllny/gis/h48c002002/lrdl.shp");
+        // final DataSource dataSource = ogr.Open("/Users/vickllny/gis/data/World_UTM_Grid_836542499432386237/World_UTM_Grid.shp");
+        final DataSource dataSource = ogr.Open("/Users/vickllny/gis/data/cds_tdsy_510100_shp/510100_32649.shp");
+        
+        final int layerCount = dataSource.GetLayerCount();
 
-        final int layerCount = dataset.GetLayerCount();
+        SpatialReference srs3857 = new SpatialReference();
+        srs3857.ImportFromEPSG(3857);
+        SpatialReference srs4326 = new SpatialReference();
+        srs4326.ImportFromEPSG(4326);
 
         Feature feature;
         Geometry geometry;
         for (int i = 0; i < layerCount; i++) {
-            final Layer layer = dataset.GetLayer(i);
+            final Layer layer = dataSource.GetLayer(i);
             SpatialReference spatialRef = layer.GetSpatialRef();
 
             System.out.println(spatialRef.ExportToPrettyWkt());
             while ((feature = layer.GetNextFeature()) != null){
                 geometry = feature.GetGeometryRef();
-                System.out.println(geometry.ExportToWkt());
+                System.out.println("111 ===>>> " + geometry.ExportToWkt());
+                //转经纬度
+                geometry.TransformTo(srs4326);
+                
+                
+                System.out.println("222 ===>>> " + geometry.ExportToWkt());
             }
         }
     }
